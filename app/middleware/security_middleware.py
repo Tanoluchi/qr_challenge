5a017dc713d3e8280e3aa7d0c793f28ff1a02578
luciano valenzuela <luciano.valenzuela@t-mobs.com>
2025-01-26 14:17:46 -0300

    fix: Publics routes

--- app/middleware/security_middleware.py
+++ app/middleware/security_middleware.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from app.helpers.auth_user import decode_token, get_token_from_headers_or_cookies, get_current_user
 
 from app.configs.logger import logger
+from app.services.user_service import UserService
 
 
 class SecurityMiddleware:
@@ -13,7 +14,7 @@ class SecurityMiddleware:
         request = Request(scope, receive)
 
         # Ignore public routes
-        public_routes = ["/login", "/register"]
+        public_routes = ["/user/login", "/user/register", "/docs", "/openapi.json"]
         if any(request.url.path.startswith(route) for route in public_routes):
             await self.app(scope, receive, send)
             return
@@ -21,9 +22,12 @@ class SecurityMiddleware:
         try:
             token = get_token_from_headers_or_cookies(request)
             payload = decode_token(token)
-            user = get_current_user(payload)
+            user = payload.get("user")
+            if not user:
+                raise HTTPException(status_code=401, detail="Invalid token payload")
+
         except Exception as e:
-            logger.error(f"Error validating token or getting user: {str(e)}")
+            logger.error(f"Error validating token: {str(e)}")
             response = JSONResponse(status_code=401, content={"detail": str(e)})
             await response(scope, receive, send)
             return
